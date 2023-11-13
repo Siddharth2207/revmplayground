e3f83b531009a00cbf056fb64b2b2b1944198802
Siddharth2207 <siddharthbhoite22@gmail.com>
2023-11-14 00:15:34 +0530

    eval

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
 
-use ethers::contract::BaseContract; 
-use ethers::abi::parse_abi;
+
+pub mod registry;
+use ethers::abi::{ParamType, Token};
 use ethers::providers::{Http, Provider};
+use registry::{IExpressionDeployerV2, IParserV1, IInterpreterV1};
+use std::sync::Arc;
+use std::convert::TryFrom;
+use std::str::FromStr;
+// use ethers::contract::BaseContract; 
+// use ethers::abi::parse_abi; 
+
 use revm::primitives::Bytes;
+use revm::primitives::keccak256;
 use revm::{
     db::{CacheDB, EmptyDB, EthersDB},
     primitives::{address, ExecutionResult, Output, TransactTo, U256},
     Database, EVM,
 };
-use std::str::FromStr;
-use std::sync::Arc;
-use std::convert::TryFrom;
-use revm::primitives::keccak256;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -19,49 +24,58 @@ async fn main() -> anyhow::Result<()> {
     let client = Provider::<Http>::try_from(
         "https://polygon.llamarpc.com",
     )?;
-    let client = Arc::new(client);
+    let client = Arc::new(client); 
 
-    // Balance Of mapping is at index 0. so slot index is keccak(0)
-    let slot_index = "18569430475105882587588266137607568536673111973893317399460219858819262702947".parse().unwrap();
+    let rain_interpreter_expression_deployer_v2 = ethers::types::H160::from_str(&String::from("0x808F049f53Ca70A2b8Ace4c117FFbec4ce77dE84")).unwrap();
+    let expression_deployer = IExpressionDeployerV2::new(rain_interpreter_expression_deployer_v2.clone(), client.clone()); 
 
-    // ERC20 contract address
-    let pool_address = address!("84342e932797fc62814189f01f0fb05f52519708"); 
+    let parser_contract = IParserV1::new(rain_interpreter_expression_deployer_v2.clone(), client.clone()); 
 
-    // generate abi for the calldata from the human readable interface
-    let abi = BaseContract::from(
-        parse_abi(&[
-            "function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast)",
-            "function balanceOf(address account) external view returns (uint256)",
-            "function totalSupply() external view returns (uint256)"
-        ])?
-    );
+    let jittery_binomial =r#"
+        input:123456,
+        binomial18-10: decimal18-scale18<0>(bitwise-count-ones(bitwise-decode<0 10>(hash(input)))),
+        noise18-1: int-mod(hash(input 0) 1e18),
+        jittery-11: decimal18-add(binomial18-10 noise18-1),
+        jittery-1: decimal18-div(jittery-11 11e18);
+    "#.to_string(); 
+    
+    let (sources, constants) = parser_contract
+        .parse(ethers::types::Bytes::from(jittery_binomial.as_bytes().to_vec()))
+        .call()
+        .await
+        .unwrap(); 
 
-    // Encode call to balanceOf function.
-    let owner_str = String::from("0xe0e0Bb15Ad2dC19e5Eaa133968e498B4D9bF24Da"); 
-    let owner_addr = ethers::types::H160::from_str(&owner_str).unwrap(); 
-    let owner_token = ethers::abi::Token::Address(owner_addr); 
-    let encoded = abi.encode("balanceOf", owner_token)?; 
-    print!("encoded : {:#?}", encoded); 
+    let min_outputs = vec![ethers::types::U256::from_dec_str("0").unwrap()]; 
+
+    let deploy_expression = expression_deployer.deploy_expression(sources, constants, min_outputs) ; 
+    let deploy_expression_bytes = deploy_expression.calldata().unwrap(); 
+
+    let rain_interpreter_expression_deployer_v2 = address!("808F049f53Ca70A2b8Ace4c117FFbec4ce77dE84"); 
+    let rain_interpreter_store = address!("97017811542bF3C4148847DC172d1a0F7e42342D"); 
+    let ir = address!("BBE3275DCD2dF953362f6eC9dB1e44A9c9EF21F2");  
+
 
     // initialize new EthersDB
     let mut ethersdb = EthersDB::new(Arc::clone(&client), None).unwrap();
 
     // query basic properties of an account incl bytecode
-    let acc_info = ethersdb.basic(pool_address).unwrap().unwrap();
+    let acc_info = ethersdb.basic(rain_interpreter_expression_deployer_v2).unwrap().unwrap();
+    let store_acc_info = ethersdb.basic(rain_interpreter_store).unwrap().unwrap(); 
+    let ir_info = ethersdb.basic(ir).unwrap().unwrap(); 
 
-    // query value of storage slot at account address
-    let value = ethersdb.storage(pool_address, slot_index).unwrap();
+
+
+    // println!("acc_info : {:#?}",acc_info);
 
     // initialise empty in-memory-db
-    let mut cache_db = CacheDB::new(EmptyDB::default());
+    let mut cache_db = CacheDB::new(EmptyDB::default()); 
 
     // insert basic account info which was generated via Web3DB with the corresponding address
-    cache_db.insert_account_info(pool_address, acc_info);
+    cache_db.insert_account_info(rain_interpreter_expression_deployer_v2, acc_info);
+    cache_db.insert_account_info(rain_interpreter_store, store_acc_info);
+    cache_db.insert_account_info(ir, ir_info);
+    
 
-    // insert our pre-loaded storage slot to the corresponding contract key (address) in the DB
-    cache_db
-        .insert_account_storage(pool_address, slot_index, value)
-        .unwrap();
 
     // initialise an empty (default) EVM
     let mut evm = EVM::new();
@@ -71,18 +85,18 @@ async fn main() -> anyhow::Result<()> {
 
     // fill in missing bits of env struct
     // change that to whatever caller you want to be
-    evm.env.tx.caller = address!("0000000000000000000000000000000000000000");
+    evm.env.tx.caller = address!("53AB61eE41FA202227Eb4e7B176208FC626DC8A9");
     // account you want to transact with
-    evm.env.tx.transact_to = TransactTo::Call(pool_address);
+    evm.env.tx.transact_to = TransactTo::Call(rain_interpreter_expression_deployer_v2);
     // calldata formed via abigen
-    evm.env.tx.data = encoded.0.into();
+    evm.env.tx.data = deploy_expression_bytes.0.into();
     // transaction value in wei
     evm.env.tx.value = U256::from(0);
 
-    // execute transaction without writing to the DB
-    let ref_tx = evm.transact_ref().unwrap();
+    // execute transaction and write it to the DB
+    let ref_tx = evm.transact_commit().unwrap();
     // select ExecutionResult struct
-    let result = ref_tx.result;
+    let result = ref_tx;
 
     // unpack output call enum into raw bytes
     let value = match result {
@@ -93,7 +107,78 @@ async fn main() -> anyhow::Result<()> {
         result => panic!("Execution failed: {result:?}"),
     };
 
-    println!("value : {:#?}",value); 
+    let decoded_data = ethers::abi::decode(&[ParamType::Address,ParamType::Address,ParamType::Address], &value).unwrap();
+
+    let interpreter_deployer =  match &decoded_data[0] {
+        Token::Address(address) => *address,
+        _ => panic!("Expression not deployed")
+    };  
+
+    let store_deployer =  match &decoded_data[1] {
+        Token::Address(address) => *address,
+        _ => panic!("Expression not deployed")
+    };  
+
+    let expression_address =  match &decoded_data[2] {
+        Token::Address(address) => *address,
+        _ => panic!("Expression not deployed")
+    };  
+    
+    let encode_dispatch = ethers::types::U256::from_dec_str("1902557528169978329301342351199330258480862274484126679039").unwrap();
+    let statenamespace = ethers::types::U256::from_dec_str("0").unwrap();  
+
+    let context: Vec<Vec<ethers::types::U256>> = vec![] ;   
+
+    let interpreter = IInterpreterV1::new(interpreter_deployer.clone(), client.clone());  
+
+    let tx = interpreter.eval(store_deployer, statenamespace, encode_dispatch, context);  
+    let tx_bytes = tx.calldata().unwrap(); 
+
+    println!("tx_bytes : {:#?}",tx_bytes); 
+
+    let rain_interpreter_np = address!("BBE3275DCD2dF953362f6eC9dB1e44A9c9EF21F2"); 
+
+   
+    // fill in missing bits of env struct
+    // change that to whatever caller you want to be
+    evm.env.tx.caller = address!("53AB61eE41FA202227Eb4e7B176208FC626DC8A9");
+    // account you want to transact with
+    evm.env.tx.transact_to = TransactTo::Call(rain_interpreter_np);
+    // calldata formed via abigen
+    evm.env.tx.data = tx_bytes.0.into();
+    // transaction value in wei
+    evm.env.tx.value = U256::from(0);
+
+    // execute transaction and write it to the DB
+    let ref_tx = evm.transact_commit().unwrap();
+    // select ExecutionResult struct
+    let result = ref_tx;
+
+    // unpack output call enum into raw bytes
+    let value2 = match result {
+        ExecutionResult::Success {
+            output: Output::Call(value),
+            ..
+        } => value,
+        result => panic!("Execution failed: {result:?}"),
+    }; 
+
+    let evaluable_config_tuple = ParamType::Tuple(
+            [
+                ParamType::Address,
+                ParamType::Bytes,
+                ParamType::Array(Box::new(ParamType::Uint(256))),
+            ]
+            .to_vec(),
+        );
+
+    let decoded_data = ethers::abi::decode(
+        &[
+            ParamType::Array(Box::new(ParamType::Uint(256))),
+            ParamType::Array(Box::new(ParamType::Uint(256)))
+        ], &value2).unwrap(); 
+
+    println!("decoded_data : {:#?}",decoded_data);
 
     Ok(())
-}
+} 
